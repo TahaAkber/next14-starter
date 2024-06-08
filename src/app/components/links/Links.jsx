9465e9c8b86e7a967386342c81edd3102267b9c1
Taha <123082592+TahaAkber@users.noreply.github.com>
2024-06-08 21:34:56 +0500

    css completed and use media query for mobile responsiive

--- src/app/components/links/Links.jsx
+++ src/app/components/links/Links.jsx
@@ -1,6 +1,9 @@
+"use client";
 import Link from "next/link";
 import styles from "./link.module.css";
 import Navlink from "../navlink/Navlink";
+import { useState } from "react";
+
 const Links = ({}) => {
   const links = [
     {
@@ -20,22 +23,51 @@ const Links = ({}) => {
       route: "/blog",
     },
   ];
-
+  const [openmenu, setopenmenu] = useState(false);
   const Session = true;
   const IsAdmin = true;
+
   return (
-    <div className={styles.logo}>
-      {links.map((item) => (
-        <Navlink items={item} key={item.title} />
-      ))}
-      {/* Most important logic of showing admin button and login and logout links and route */}
-      {Session ? (
-        <>
-          {IsAdmin && <Navlink items={{ title: "Admin", route: "/admin" }} />}
-          <button>Logout</button>
-        </>
-      ) : (
-        <Navlink items={{ title: "Login", route: "/login" }} />
+    <div className={styles.container}>
+      <div className={styles.navbar}>
+        <div className={styles.links}>
+          {links.map((item) => (
+            <Navlink items={item} key={item.title} />
+          ))}
+          {Session ? (
+            <>
+              {IsAdmin && (
+                <Navlink items={{ title: "Admin", route: "/admin" }} />
+              )}
+              <button className={styles.logout}>Logout</button>
+            </>
+          ) : (
+            <Navlink items={{ title: "Login", route: "/login" }} />
+          )}
+        </div>
+        <button
+          onClick={() => setopenmenu((prev) => !prev)}
+          className={styles.menubutton}
+        >
+          Menu
+        </button>
+      </div>
+      {openmenu && (
+        <div className={styles.mobile}>
+          {links.map((item) => (
+            <Navlink items={item} key={item.title} />
+          ))}
+          {Session ? (
+            <>
+              {IsAdmin && (
+                <Navlink items={{ title: "Admin", route: "/admin" }} />
+              )}
+              <button className={styles.logout}>Logout</button>
+            </>
+          ) : (
+            <Navlink items={{ title: "Login", route: "/login" }} />
+          )}
+        </div>
       )}
     </div>
   );
