adca009b744ea614be514b9a4d7f9717353aff37
Taha <123082592+TahaAkber@users.noreply.github.com>
2024-06-08 22:22:42 +0500

    ABOUT PAGE IMAGE

--- src/app/(pages)/about/page.jsx
+++ src/app/(pages)/about/page.jsx
@@ -1,5 +1,14 @@
+import Image from "next/image";
+import "./about.css";
+
 const aboutpage = () => {
-  return <div>aboutpage</div>;
+  return (
+    <div>
+      <div className="imgcontainer">
+        <Image src="/about.png" alt="image" />
+      </div>
+    </div>
+  );
 };
 
 export default aboutpage;
