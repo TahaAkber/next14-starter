688d3fe8a99d6af1ec4f8da9a7fbe81f0a073bfa
Taha <123082592+TahaAkber@users.noreply.github.com>
2024-06-09 16:14:37 +0500

    Design of about page completed

--- src/app/(pages)/about/page.jsx
+++ src/app/(pages)/about/page.jsx
@@ -1,9 +1,36 @@
 import Image from "next/image";
-
+import styles from "./about.module.css";
 const aboutpage = () => {
   return (
-    <div>
-      <div className="imgcontainer">
+    <div className={styles.container}>
+      <div className={styles.imgcontainer}>
+        <div className={styles.about}>
+          <h1 className={styles.head}>About Us</h1>
+          <p className={styles.text}>
+            We create digital ideas that are bigger, bolder, braver and better
+          </p>
+          <p className={styles.desc}>
+            We create digital ideas that are bigger, bolder, braver and better.
+            We believe in good ideas flexibility and precission We’re world’s
+            Our Special Team best consulting & finance solution provider. Wide
+            range of web and software development services.
+          </p>
+
+          <div className={styles.boxes}>
+            <div className={styles.box}>
+              <h1>10 K+</h1>
+              <p>Year of experience</p>
+            </div>
+            <div className={styles.box}>
+              <h1>10 K+</h1>
+              <p>Year of experience</p>
+            </div>
+            <div className={styles.box}>
+              <h1>10 K+</h1>
+              <p>Year of experience</p>
+            </div>
+          </div>
+        </div>
         <Image
           src="/about.png"
           alt="image"
