915756d9688a0e2595a4b6bd0cafeeee81b4b6c9
Taha <123082592+TahaAkber@users.noreply.github.com>
2024-06-09 16:37:52 +0500

    working on a blog page with postcard

--- src/app/(pages)/blog/page.jsx
+++ src/app/(pages)/blog/page.jsx
@@ -1,5 +1,13 @@
+import Postcard from "@/app/components/postcard/Postcard";
+import styles from "./blog.module.css";
 const blogpage = () => {
-  return <div>blogpage</div>;
+  return (
+    <div className={styles.container}>
+      <Postcard />
+      <Postcard />
+      <Postcard />
+    </div>
+  );
 };
 
 export default blogpage;
---
+++ src/app/components/postcard/Postcard.jsx
@@ -0,0 +1,6 @@
+import styles from "./postcard.module.css";
+const Postcard = ({}) => {
+  return <div className={styles.container}>Postcard</div>;
+};
+
+export default Postcard;
