1663fedd85901466fbc78c93dcf63fc77702d38e
Taha <123082592+TahaAkber@users.noreply.github.com>
2024-06-09 19:49:08 +0500

    Update Postcard.jsx

--- src/app/components/postcard/Postcard.jsx
+++ src/app/components/postcard/Postcard.jsx
@@ -1,9 +1,20 @@
+import Link from "next/link";
+import Image from "next/image";
 import styles from "./postcard.module.css";
 const Postcard = ({}) => {
   return (
     <div className={styles.container}>
-      <div className={styles.top}></div>
-      <div className={styles.bottom}></div>
+      <div className={styles.top}>
+        <div className={styles.imgcontainer}>
+          {/* <Image src= alt="" fill className={styles.img} /> */}
+        </div>
+        <span className={styles.date}>01.01.2024</span>
+      </div>
+      <div className={styles.bottom}>
+        <h1 className={styles.title}>Title</h1>
+        <p className={styles.desc}>Desc</p>
+        <Link href="/blog/post">READ MORE</Link>
+      </div>
     </div>
   );
 };
