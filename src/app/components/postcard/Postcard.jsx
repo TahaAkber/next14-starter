12c727b8a042958dab12cf327c3acd94732d03c3
Taha <123082592+TahaAkber@users.noreply.github.com>
2024-06-09 17:59:58 +0500

    Update Postcard.jsx

--- src/app/components/postcard/Postcard.jsx
+++ src/app/components/postcard/Postcard.jsx
@@ -1,6 +1,11 @@
 import styles from "./postcard.module.css";
 const Postcard = ({}) => {
-  return <div className={styles.container}>Postcard</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.top}></div>
+      <div className={styles.bottom}></div>
+    </div>
+  );
 };
 
 export default Postcard;
